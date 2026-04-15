8dc8fe976ebc804b2d0efe7b63f9de0204bb4814
Sandeep-Kumar-046 <avnsandeep07@gmail.com>
2026-04-15 15:40:15 +0530

    Scripts added to the  calculator project & Stage 1: Adding number values

---
+++ Projects/Calculator/script.js
@@ -0,0 +1,22 @@
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+let firstValue = 0;
+let awaitingNextValue = false;
+function addNumberValue(number)
+{
+  if(awaitingNextValue){
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  }
+  else{
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === "0" ? number : displayValue + number;
+  }
+}
+inputBtns.forEach((inputBtn)=>{
+  if(inputBtn.classList.length === 0)
+  {
+    inputBtn.addEventListener("click",()=>addNumberValue(inputBtn.value));
+  }
+});
